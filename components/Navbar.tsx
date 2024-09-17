9eca8de3df0c832ab193de534dd0969767cb2e3d
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-09-17 05:05:49 -0400

    Completes navbar
    Adds all navbar options and responsiveness.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { MdOutlineMenu } from "react-icons/md";
 import { FaInbox } from "react-icons/fa";
@@ -8,30 +8,54 @@ import { GoRepoForked } from "react-icons/go";
 import { BsRecordCircle } from "react-icons/bs";
 import { FaCaretDown } from "react-icons/fa6";
 import { IoMdAdd } from "react-icons/io";
-
+import { IoSearch } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className='border-b border-gray-700 py-4'>
-      <div className='px-4 flex justify-between'>
-        <div className='flex items-center gap-3'>
-          <MdOutlineMenu className='text-[30px] border border-gray-700 rounded-md p-1' />
-          <Link href='/'><FaGithub className='text-3xl' /></Link>
-          <p className='font-semibold text-sm'>Dashboard</p>
+    <nav className="border-b border-gray-700 py-4">
+      <div className="px-4 flex justify-between">
+        <div className="flex items-center gap-3">
+          <MdOutlineMenu className="text-[30px] border border-gray-700 rounded-md p-1" />
+          <Link href="/">
+            <FaGithub className="text-3xl" />
+          </Link>
+          <p className="font-semibold text-sm">Dashboard</p>
         </div>
-        <div className='flex items-center gap-3 text-gray-400'>
-          <div className='border border-gray-600 flex gap-2 p-2 text-xl rounded-lg'>
+        <div className="flex items-center gap-3 text-gray-400">
+          <div className="flex gap-2 px-2 items-center rounded-xl border border-gray-600 max-md:py-2">
+            <IoSearch />
+            <input
+              type="text"
+              className="bg-black max-md:hidden rounded-lg py-1"
+              placeholder="Type / to search"
+            />
+          </div>
+
+          <div className="border border-gray-600 flex gap-2 p-2 text-md rounded-lg max-sm:hidden">
             <IoMdAdd />
             <FaCaretDown />
           </div>
-          <BsRecordCircle />
-          <GoRepoForked />
-          <FaInbox />
-          <Image src='/profile.jpg' alt='Profile' width={35} height={35} className='rounded-full' />
+          <div className="border border-gray-600 flex gap-2 p-2 text-md rounded-lg max-sm:hidden">
+            <BsRecordCircle />
+          </div>
+
+          <div className="border border-gray-600 flex gap-2 p-2 text-md rounded-lg max-sm:hidden">
+            <GoRepoForked />
+          </div>
+          <div className="border border-gray-600 flex gap-2 p-2 text-md rounded-lg">
+            <FaInbox />
+          </div>
+          <Image
+            src="/profile.jpg"
+            alt="Profile"
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
