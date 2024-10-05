e44d5dea99c1be24f0d5353787d6e979dcce6175
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-10-05 12:43:14 -0400

    Adds sections to the landing page.

--- components/External/Hero.tsx
+++ components/External/Hero.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import React from "react";
 import { PiBracketsCurlyBold } from "react-icons/pi";
+import { GoBriefcase } from "react-icons/go";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Hero = () => {
   return (
@@ -25,7 +27,16 @@ const Hero = () => {
             <PiBracketsCurlyBold className="text-gray-200 text-3xl" />
           </div>
           <div className="mt-2">
-            <div className="w-[4px] h-[450px]  bg-gradient-to-t from-[#7b72ff] to-red-400 mt-2"></div>
+            <div className="w-[4px] h-[420px]  bg-gradient-to-t from-[#7b72ff] to-red-400 mt-2"></div>
+          </div>
+          <div className="mt-4">
+            <GoBriefcase className="text-gray-200 text-3xl" />
+          </div>
+          <div className="mt-2">
+            <div className="w-[4px] h-[220px]  bg-gradient-to-b from-green-500"></div>
+          </div>
+          <div className="mt-[700px]">
+            <div className="w-[4px] h-[220px]  bg-gradient-to-t from-green-500"></div>
           </div>
         </div>
 
@@ -68,6 +79,32 @@ const Hero = () => {
               <img src="/pg.svg" alt="" />
               <img src="/tellus.svg" alt="" />
             </div>
+          </div>
+          <div className="mt-[185px]">
+            <h2 className="text-white text-3xl font-semibold max-md:text-4xl">
+              Productivity
+            </h2>
+            <h3 className="text-green-500 font-semibold text-5xl">
+              Accelerate innovation
+            </h3>
+            <h3 className="text-white font-semibold text-5xl">
+              Our AI-Powered platform increases the <br />
+              pace of software development.
+            </h3>
+          </div>
+          <div className="mt-20 relative -left-[100px]">
+            <img src="code.png" alt="" />
+          </div>
+          <div className="mt-[60px]">
+            <h3 className="text-gray-400 text-3xl">
+              <span className="font-semibold text-white">GitHub Copilot</span>
+              empowers developers to <br /> complete tasks 55% faster with
+              <br /> contextualized AI coding assistance across <br />
+              workflows.
+            </h3>
+            <button className="font-bold text-white text-xl mt-2 flex gap-2 items-center hover:text-gray-500">
+              Explore Github Copilot <IoIosArrowForward />
+            </button>
           </div>
         </div>
       </div>
